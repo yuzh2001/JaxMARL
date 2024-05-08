02307dfcc468750640966cd5c73527d39531ceca
Tobias Gessler <gessler.tobi@gmail.com>
2024-05-08 11:23:30 +0100

    feat(overcooked): Restructure layout

--- jaxmarl/environments/overcooked_v2/layouts.py
+++ jaxmarl/environments/overcooked_v2/layouts.py
@@ -1,8 +1,7 @@
-from flax.core.frozen_dict import FrozenDict
-from jaxmarl.environments.overcooked_v2.common import Direction, Position, StaticObject
-from flax import struct
+from jaxmarl.environments.overcooked_v2.common import StaticObject
 import numpy as np
-import jax.numpy as jnp
+from typing import List, Tuple, Optional
+from dataclasses import dataclass
 
 cramped_room = """
 WWPWW
@@ -51,22 +50,24 @@ WWWOOWWW
 """
 
 
-@struct.dataclass
+@dataclass
 class Layout:
-    # agent positions list of positions num_agents x 2 (x, y)
-    agent_positions: jnp.ndarray
+    # agent positions list of positions, tuples (x, y)
+    agent_positions: List[Tuple[int, int]]
 
     # width x height grid with static items
     static_objects: np.ndarray
     width: int
     height: int
 
-    recipe: np.ndarray
+    # If none recipe will be randomized on reset
+    # If present recipe should be a list of ingredient indices, max 3 ingredients per recipe
+    recipe: Optional[List[int]]
 
     num_ingredients: int
 
 
-def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
+def layout_grid_to_dict(grid, recipe=None):
     """Assumes `grid` is string representation of the layout, with 1 line per row, and the following symbols:
     W: wall
     A: agent
@@ -79,6 +80,11 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
 
     Depricated:
     O: onion pile - will be interpreted as I0
+
+
+    If `recipe` is provided, it should be a list of ingredient indices, max 3 ingredients per recipe
+    If `recipe` is not provided, the recipe will be randomized on reset.
+    If the layout does not have a recipe indicator, a fixed `recipe` must be provided.
     """
 
     rows = grid.split("\n")
@@ -89,7 +95,7 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
         rows = rows[:-1]
 
     row_lens = [len(row) for row in rows]
-    static_objects = np.zeros((len(rows), max(row_lens)), dtype=np.int32)
+    static_objects = np.zeros((len(rows), max(row_lens)))
 
     char_to_static_item = {
         " ": StaticObject.EMPTY,
@@ -107,6 +113,7 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
 
     max_width = 0
     num_ingredients = 0
+    includes_recipe_indicator = False
     for r, row in enumerate(rows):
         j = 0
         c = 0
@@ -121,14 +128,18 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
                 char = "I0"
 
             if char == "A":
-                agent_pos = [c, r]
+                agent_pos = (c, r)
                 agent_positions.append(agent_pos)
 
             obj = char_to_static_item.get(char, StaticObject.EMPTY)
             static_objects[r, c] = obj
+
             if StaticObject.is_ingredient_pile(obj):
                 ingredient_idx = obj - StaticObject.INGREDIENT_PILE_BASE
                 num_ingredients = max(num_ingredients, ingredient_idx + 1)
+
+            if obj == StaticObject.RECIPE_INDICATOR:
+                includes_recipe_indicator = True
 
             j += 1
             c += 1
@@ -136,17 +147,22 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
 
     # TODO: add some sanity checks - e.g. agent must exist, surrounded by walls, etc.
 
-    if not (0 < len(recipe) <= 3):
-        raise ValueError("Recipe must be a list of length 1, 2, or 3")
-    if [i for i in recipe if i < 0 or i >= num_ingredients]:
-        raise ValueError("Invalid ingredient index in recipe")
+    if recipe is not None:
+        if not (0 < len(recipe) <= 3):
+            raise ValueError("Recipe must be a list of length 1, 2, or 3")
+        if [i for i in recipe if i < 0 or i >= num_ingredients]:
+            raise ValueError("Invalid ingredient index in recipe")
+    elif not includes_recipe_indicator:
+        raise ValueError(
+            "Layout does not include a recipe indicator, a fixed recipe must be provided"
+        )
 
     layout = Layout(
-        agent_positions=jnp.array(agent_positions),
+        agent_positions=agent_positions,
         static_objects=static_objects[:, :max_width],
         width=max_width,
         height=len(rows),
-        recipe=np.array(recipe),
+        recipe=recipe,
         num_ingredients=num_ingredients,
     )
 
@@ -154,10 +170,10 @@ def layout_grid_to_dict(grid, recipe=[0, 0, 0]):
 
 
 overcooked_v2_layouts = {
-    "cramped_room": layout_grid_to_dict(cramped_room),
-    "cramped_room_v2": layout_grid_to_dict(cramped_room_v2, recipe=[0, 0, 1]),
-    "asymm_advantages": layout_grid_to_dict(asymm_advantages),
-    "coord_ring": layout_grid_to_dict(coord_ring),
-    "forced_coord": layout_grid_to_dict(forced_coord),
-    "counter_circuit": layout_grid_to_dict(counter_circuit_grid),
+    "cramped_room": layout_grid_to_dict(cramped_room, recipe=[0, 0, 0]),
+    "cramped_room_v2": layout_grid_to_dict(cramped_room_v2),
+    "asymm_advantages": layout_grid_to_dict(asymm_advantages, recipe=[0, 0, 0]),
+    "coord_ring": layout_grid_to_dict(coord_ring, recipe=[0, 0, 0]),
+    "forced_coord": layout_grid_to_dict(forced_coord, recipe=[0, 0, 0]),
+    "counter_circuit": layout_grid_to_dict(counter_circuit_grid, recipe=[0, 0, 0]),
 }
