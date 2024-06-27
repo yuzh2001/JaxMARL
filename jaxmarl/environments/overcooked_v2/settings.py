52bbb93c52f8b9db660d64f0832e1c870608fb02
Tobias Gessler <gessler.tobi@gmail.com>
2024-06-27 16:57:23 +0100

    feat(overcooked): move seetings to file

---
+++ jaxmarl/environments/overcooked_v2/settings.py
@@ -0,0 +1,9 @@
+URGENCY_CUTOFF = 40  # When this many time steps remain, the urgency layer is flipped on
+DELIVERY_REWARD = 20
+POT_COOK_TIME = 20  # Time it takes to cook a pot
+SHAPED_REWARDS = {
+    "PLACEMENT_IN_POT": 3,
+    "POT_START_COOKING": 5,
+    "DISH_PICKUP": 5,
+    "PLATE_PICKUP": 3,
+}
